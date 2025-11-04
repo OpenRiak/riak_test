e53bc2f175ba3355fe477bf6cbdec8b33659c273
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-11-04 16:50:36 +0000

    Add metadata_version_change test
    Adds a test to show that mixing nodes in the cluster with v0 metadata (dict-based) and v1 (map based) doesn't cause any issues.

--- tests/nextgenrepl_rtq_auto.erl
+++ tests/nextgenrepl_rtq_auto.erl
@@ -41,8 +41,6 @@
 -define(COMMMON_VAL_MOD, <<"CommonValueToWriteForAllModifiedObjects">>).
 
 -define(REPL_SLEEP, 2048).
-    % May need to wait for 2 x the 1024ms max sleep time of a snk worker
--define(WAIT_LOOPS, 12).
 
 -define(STATS_WAIT, 1000).
 
